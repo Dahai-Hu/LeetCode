b45d3eedbc56cdc769897b1548c749835ee1aed0
HuRuSea <hurusea1@gmail.com>
2020-03-29 12:53:03 +0800

    输入两个递增排序的链表，合并这两个链表并使新链表中的节点仍然是递增排序的。

--- src/day0329/Solution.java
+++ src/day0329/Solution.java
@@ -152,4 +152,45 @@ public class Solution {
         }
     }
 
+    /**
+     *
+     * @param head
+     * @return
+     */
+    public ListNode reverseList(ListNode head) {
+        ListNode cur = head;
+        ListNode pre = null;
+        ListNode temp = null;
+        while (cur != null) {
+            temp=cur.next;
+            cur.next=pre;
+            pre=cur;
+            cur=temp;
+        }
+        return pre;
+    }
+
+    /**
+     * 输入两个递增排序的链表，合并这两个链表并使新链表中的节点仍然是递增排序的。
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dum = new ListNode(0), cur = dum;
+        while(l1 != null && l2 != null) {
+            if(l1.val < l2.val) {
+                cur.next = l1;
+                l1 = l1.next;
+            }
+            else {
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+        cur.next = l1 != null ? l1 : l2;
+        return dum.next;
+    }
+
 }
