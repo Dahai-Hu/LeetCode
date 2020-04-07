45ce830cd5e9c6945b0a374b26ddb6733eb95122
HuRuSea <hurusea1@gmail.com>
2020-04-07 21:50:47 +0800

    网易笔试题

--- src/day0407/Solution.java
+++ src/day0407/Solution.java
@@ -3,6 +3,7 @@ package day0407;
 import org.junit.Test;
 
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.util.Queue;
 
 /**
@@ -117,6 +118,45 @@ public class Solution {
         int[] nums = {1, 2, 3, 2, 2, 2, 5, 4, 2};
         int result = majorityElement(nums);
         System.out.println(result);
+    }
+
+    /**
+     * 输入整数数组 arr ，找出其中最小的 k 个数。
+     * 例如，输入4、5、1、6、2、7、3、8这8个数字，
+     * 则最小的4个数字是1、2、3、4。
+     * @param arr
+     * @param k
+     * @return
+     * 1.定义一个长度为k的数组res
+     * 2.对arr进行遍历，将第一个元素放进res
+     * 3.比较arr元素和res元素，保证res中升序
+     * 4.res满了之后，将arr中的元素和res中第一个进行比较
+     */
+    public int[] getLeastNumbers(int[] arr, int k) {
+        if (k == 0) {
+            return new int[0];
+        }
+        // 使用一个最大堆（大顶堆）
+        // Java 的 PriorityQueue 默认是小顶堆，添加 comparator 参数使其变成最大堆
+        Queue<Integer> heap = new PriorityQueue<>(k, (i1, i2) -> Integer.compare(i2, i1));
+
+        for (int e : arr) {
+            // 当前数字小于堆顶元素才会入堆
+            if (heap.isEmpty() || heap.size() < k || e < heap.peek()) {
+                heap.offer(e);
+            }
+            if (heap.size() > k) {
+                heap.poll(); // 删除堆顶最大元素
+            }
+        }
+
+        // 将堆中的元素存入数组
+        int[] res = new int[heap.size()];
+        int j = 0;
+        for (int e : heap) {
+            res[j++] = e;
+        }
+        return res;
     }
 
 }
